6d35081fe9e15bf15015bb39244794d0fc7cee05
Arya Damle <144161572+AryaDamle@users.noreply.github.com>
2025-03-11 21:32:41 +0530

    Create Dashboard.jsx

---
+++ frontend/Dashboard.jsx
@@ -0,0 +1,5 @@
+/* Login page : Email/Phone, Passowrd,Forget password,( Login with google, Login with facebook, Login with ios) ,  Sign Up.
+Forget password: Phone/ Email/ Username , New password, Confirm password, Save , Go to login page
+Sign Up :  Sign up with google/microsoft/facebook/ios,  First name, Middle name(opt), Last name, phone, tel-phone(opt), email, dob, password, confirm password , terms & conditions, sign up , remember me 
+Onboarding : 
+Dashboard: calender, reminders , */
