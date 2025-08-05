60fa5d44e7b9b96b9696bc2984cd986a1da34782
Yashika <yashika0718@gmail.com>
2025-08-05 12:35:47 +0530

    Updated auth.js

--- AT learning dashboard/frontend/src/api/auth.js
+++ AT learning dashboard/frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 // src/api/auth.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
 
 // ---- Keys ----
 const TOKEN_KEY = "token";
